a86afc36a216e11e372c55fb03cbbe7b6e39003d
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 21:23:46 -0800

    css overhaul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Attribution from "@/components/attribution";
 import SearchBar from "@/components/searchbar";
 import { useState } from "react";
 import type { Product, RestaurantData } from "../types";
+import Image from "next/image";
 
 interface Restaurant {
   name: string;
@@ -28,12 +29,47 @@ export default function Home() {
   };
 
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <nav>
+          <ul>
+            <br />
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            {/* <li>
+              <a href="restaurant.html">Restaurants</a>
+            </li>
+            <li>
+              <a href="nutrition.html">Nutrition</a>
+            </li> */}
+            <div className="search">
+              <li>
+                <input type="text" placeholder="Search..." name="search" />
+              </li>
+              <li>
+                <button type="submit">
+                  <i className="search-">Button</i>
+                </button>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
         <SearchBar handleData={handler} />
         {restaurants.length}
+        <br />
+        <h1>Improving Restaurant Choices Based On Nutrition</h1>
+        <p>You should always dine perfect.</p>
+        <br />
+        <hr />
+        <img src="/restaurant.jpg" alt="restaurant image" />
       </div>
       <Attribution />
-    </main>
+    </div>
   );
 }
